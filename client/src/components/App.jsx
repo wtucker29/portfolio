5b70081c875a39007ed8e282e4748386743892eb
William Tucker <william.tucker29@gmail.com>
2024-06-13 11:05:50 -0500

    Added index.html, index.css, components folder with App.jsx

---
+++ client/src/components/App.jsx
@@ -0,0 +1,23 @@
+import { lazy, Suspense, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+export default function App() {
+  return (
+    <>
+    <header>
+      <nav>
+        <h1>William Tucker</h1>
+        <ul>
+          <li><a href="/about">About</a></li>
+          <li><a href="/experience">Experience</a></li>
+          <li><a href="/projects">Projects</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <Suspense fallback={<p>Loading...</p>}>{renderView()}</Suspense>
+    </main>
+    </>
+  )
+};
---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./assets/index.css";
+import { render } from "react-dom";
+import App from "./components/App.jsx";
+
+const root = document.createElement("div");
+root.setAttribute("id", "root");
+document.body.appendChild(root);
+
+render(<App />, root);
