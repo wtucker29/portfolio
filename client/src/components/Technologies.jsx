9371f2a780a74c16b2f5c3c46ff7bb1d031e5a2d
William Tucker <william.tucker29@gmail.com>
2024-07-01 14:05:19 -0700

    Changed class names

--- client/src/components/Technologies.jsx
+++ client/src/components/Technologies.jsx
@@ -5,72 +5,12 @@ export default function Technologies() {
     <section id="technologies" className="section">
       <div className="container">
         <h2 className="title">Technologies</h2>
-          <div className="columns">
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=react" alt="React icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=html" alt="HTML icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=css" alt="CSS icon" />
-            </div>
+        <div className="icon-container">
+          <div className="icon-rows">
+            <img src="https://skillicons.dev/icons?i=js,html,css,jquery,react,express,babel,jest,vscode,bash,git,github,aws" />
+            <img src="https://skillicons.dev/icons?i=mongodb,mysql,nodejs,postgres,postman,replit,supabase,webpack,figma,nginx,npm,redis,vite" />
           </div>
-          <div className="columns">
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=js" alt="JavaScript icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=ts" alt="TypeScript icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=py" alt="Python icon" />
-            </div>
-          </div>
-          <div className="columns">
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=mongodb" alt="MongoDB icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=postgres" alt="Postgres icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=mysql" alt="MySQL icon" />
-            </div>
-          </div>
-          <div className="columns">
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=nodejs" alt="Node.js icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=express" alt="Express icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=vite" alt="Vite icon" />
-            </div>
-          </div>
-          <div className="columns">
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=aws" alt="AWS icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=redis" alt="Redis icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=github" alt="GitHub icon" />
-            </div>
-          </div>
-          <div className="columns">
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=django" alt="Django icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=docker" alt="Docker icon" />
-            </div>
-            <div className="column is-4">
-              <img src="https://skillicons.dev/icons?i=postman" alt="Postman icon" />
-            </div>
-          </div>
+        </div>
       </div>
     </section>
   );
