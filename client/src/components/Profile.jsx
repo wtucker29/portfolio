4ab8f7c6ee7e316e687d57ad3c2411c8f92deff5
William Tucker <william.tucker29@gmail.com>
2024-07-01 10:49:55 -0700

    Updated Profile to utilize Bulma column styling

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -4,29 +4,35 @@ export default function Profile() {
   return (
     <section id="profile" className="section">
       <div className="container">
-        <div className="pic-container">
-          <img src="../assets/profile-pic.jpg" alt="William Tucker profile picture" />
-        </div>
-        <div className="profile-info">
-          <h2 className="title">William Tucker</h2>
-          <p className="profile-text">Full Stack Developer</p>
-          <div className="btn-container">
-            <button
-              className="button is-primary is-outlined is-medium"
-              onClick={() => {
-                window.open("/assets/William_Tucker_FullStack_Resume.pdf");
-              }}
-            >
-              Download CV
-            </button>
-            <button
-              className="button is-primary is-outlined is-medium"
-              onClick={() => {
-                document.getElementById("contact").scrollIntoView();
-              }}
-            >
-              Contact Info
-            </button>
+        <div className="columns is-vcentered">
+          <div className="column is-one-quarter">
+            <div className="pic-container">
+              <img src="/assets/William_Tucker_Photo.jpg" alt="William Tucker profile picture" className="profile-pic" />
+            </div>
+          </div>
+          <div className="column">
+            <div className="profile-info">
+              <h2 className="title">William Tucker</h2>
+              <p className="profile-text">Full Stack Developer</p>
+              <div className="btn-container">
+                <button
+                  className="button is-primary is-outlined is-medium"
+                  onClick={() => {
+                    window.open("/assets/William_Tucker_FullStack_Resume.pdf");
+                  }}
+                >
+                  Download Resume
+                </button>
+                <button
+                  className="button is-primary is-outlined is-medium"
+                  onClick={() => {
+                    document.getElementById("contact").scrollIntoView();
+                  }}
+                >
+                  Contact Info
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
