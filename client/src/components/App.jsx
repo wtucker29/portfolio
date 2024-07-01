a81fe281b003c45135ba0be425cde7bfebf533b6
William Tucker <william.tucker29@gmail.com>
2024-07-01 13:08:45 -0700

    Changed Experience to Technologies

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense, useState } from "react";
 import Profile from "./Profile.jsx";
 import About from "./About.jsx";
-import Experience from "./Experience.jsx";
+import Technologies from "./Experience.jsx";
 import Projects from "./Projects.jsx";
 import Contact from "./Contact.jsx";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -18,7 +18,7 @@ export default function App() {
           <div className="column is-flex is-justify-content-flex-end">
             <ul className="navbar-menu is-flex">
               <li className="navbar-item"><a href="#about">About</a></li>
-              <li className="navbar-item"><a href="#experience">Experience</a></li>
+              <li className="navbar-item"><a href="#technologies">Technologies</a></li>
               <li className="navbar-item"><a href="#projects">Projects</a></li>
               <li className="navbar-item"><a href="#contact">Contact</a></li>
             </ul>
@@ -29,7 +29,7 @@ export default function App() {
     <main className="container my-6">
       <Profile />
       <About />
-      <Experience />
+      <Technologies />
       <Projects />
       <Contact />
     </main>
