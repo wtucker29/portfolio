f360d14cab8b247c90aa596028d5ff529974852f
William Tucker <william.tucker29@gmail.com>
2024-07-02 14:39:37 -0700

    Added alt text to images

--- client/src/components/Technologies.jsx
+++ client/src/components/Technologies.jsx
@@ -7,8 +7,8 @@ export default function Technologies() {
         <h2 className="title">Technologies</h2>
         <div className="icon-container">
           <div className="icon-rows">
-            <img src="https://skillicons.dev/icons?i=js,html,css,jquery,react,express,babel,jest,vscode,bash,git,github,aws" />
-            <img src="https://skillicons.dev/icons?i=mongodb,mysql,nodejs,postgres,postman,replit,supabase,webpack,figma,nginx,npm,redis,vite" />
+            <img src="https://skillicons.dev/icons?i=js,html,css,jquery,react,express,babel,jest,vscode,bash,git,github,aws" alt="tech icons" />
+            <img src="https://skillicons.dev/icons?i=mongodb,mysql,nodejs,postgres,postman,replit,supabase,webpack,figma,nginx,npm,redis,vite" alt="tech icons" />
           </div>
         </div>
       </div>
