a57e54081d29f83aeb2eb9c829cfe59a061e2eff
William Tucker <william.tucker29@gmail.com>
2024-07-02 10:26:29 -0700

    Created Contact component. Added mailto link to email me and a link to my linkedin profile

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Contact() {
   return (
@@ -6,7 +9,23 @@ export default function Contact() {
       <div className="container">
         <h2 className="title">Contact Me</h2>
         <div className="columns">
-          <div className="column is-3">
+          <div className="column has-text-centered">
+            <span className="contact-email">
+              <p>
+                <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                <a href="mailto:william.tucker29@gmail.com?subject=Contact%20from%20Portfolio">
+                  william.tucker29@gmail.com
+                </a>
+              </p>
+            </span>
+            <span className="contact-linkedin">
+              <p>
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                <a href="https://www.linkedin.com/in/william-h-tucker-iv" target="_blank" rel="noreferrer">
+                  LinkedIn
+                </a>
+              </p>
+            </span>
           </div>
         </div>
       </div>
