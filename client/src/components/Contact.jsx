015b3e9197c264904f1a12178f3ffafedd2bdb3b
William Tucker <william.tucker29@gmail.com>
2024-07-02 14:22:50 -0700

    Added Contact Me section. Includes link to email and linkedin.

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -8,24 +8,20 @@ export default function Contact() {
     <section id="contact" className="section">
       <div className="container">
         <h2 className="title">Contact Me</h2>
-        <div className="columns">
-          <div className="column has-text-centered">
-            <span className="contact-email">
-              <p>
-                <FontAwesomeIcon icon={faEnvelope} size="2x" />
-                <a href="mailto:william.tucker29@gmail.com?subject=Contact%20from%20Portfolio">
-                  william.tucker29@gmail.com
-                </a>
-              </p>
-            </span>
-            <span className="contact-linkedin">
-              <p>
-                <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                <a href="https://www.linkedin.com/in/william-h-tucker-iv" target="_blank" rel="noreferrer">
-                  LinkedIn
-                </a>
-              </p>
-            </span>
+        <div className="columns is-centered">
+          <div className="column is-narrow">
+            <div className="contact-item">
+              <FontAwesomeIcon icon={faEnvelope} size="2x" style={{ color: "white" }}/>
+              <a href="mailto:william.tucker29@gmail.com?subject=Contact%20from%20Portfolio" className="contact-link">
+                william.tucker29@gmail.com
+              </a>
+            </div>
+            <div className="contact-item">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" style={{ color: "white" }}/>
+              <a href="https://www.linkedin.com/in/william-h-tucker-iv" target="_blank" rel="noreferrer" className="contact-link">
+                LinkedIn
+              </a>
+            </div>
           </div>
         </div>
       </div>
