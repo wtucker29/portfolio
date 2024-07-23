2e0dc0f82063dd071b65f1645f6e9c1eff0b2e32
William Tucker <william.tucker29@gmail.com>
2024-07-23 14:16:18 -0500

    Imported Header component. Removed old code for navbar. That code is now in the Header component.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,29 +6,13 @@ import Technologies from "./Technologies.jsx";
 import Projects from "./Projects.jsx";
 import Contact from "./Contact.jsx";
 import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <header className="navbar is-light">
-        <div className="container">
-          <div className="columns is-vcentered">
-            <div className="column">
-              <h1 className="title is-4 has-text-primary-00">William Tucker</h1>
-            </div>
-            <div className="column is-flex is-justify-content-flex-end">
-              <ul className="navbar-menu is-flex">
-                <li className="navbar-item"><a href="#about">About</a></li>
-                <li className="navbar-item"><a href="#experience">Experience</a></li>
-                <li className="navbar-item"><a href="#technologies">Technologies</a></li>
-                <li className="navbar-item"><a href="#projects">Projects</a></li>
-                <li className="navbar-item"><a href="#contact">Contact</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
       <main className="container my-6">
         <Profile />
         <About />
