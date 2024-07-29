74b436657f2189c666f716a12403a3d36e3ef93b
William Tucker <william.tucker29@gmail.com>
2024-07-29 10:50:51 -0500

    Changed download resume button endpoint to new resume

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -20,7 +20,7 @@ export default function Profile() {
                 <button
                   className="button is-link is-focused is-medium is-responsive"
                   onClick={() => {
-                    window.open("/assets/William_Tucker_FullStack_Resume.pdf");
+                    window.open("/assets/William-Tucker-Software-Engineer-Resume.pdf");
                   }}
                 >
                   Download Resume
