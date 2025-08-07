96404edbd14e7ede10f3a3c10bc9643b90b4c157
William Tucker <william.tucker29@gmail.com>
2025-08-07 14:26:50 -0500

    Updated dates on resume.

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -20,7 +20,7 @@ export default function Profile() {
                 <button
                   className="button is-link is-focused is-medium is-responsive"
                   onClick={() => {
-                    window.open("/assets/WilliamTucker_Resume.pdf", "_blank");
+                    window.open("/assets/WilliamTuckerResume.pdf", "_blank");
                   }}
                 >
                   Download Resume
